6a5fb3f0b446b2c558ad20e93086e07ae3d5ec92
Adérito Muniunga <50484751+Muniunga@users.noreply.github.com>
2021-07-13 13:58:26 +0100

    w

---
+++ script.js
@@ -0,0 +1,22 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+};
+
+function criarBg() {
+  context.fillStyle = "lightgreen";
+  context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha(params) {
+  for (i = 0; i < snake.length; i++) {
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
+criarBg();
